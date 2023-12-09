93cc1cae1f9f8e10c12eff71c4d462476ac884cc
Kalpita Mandal <kalpita103@gmail.com>
2023-12-09 13:00:38 +0530

    Adding basic template for anon aadhar

--- consts/index.ts
+++ consts/index.ts
@@ -2,4 +2,5 @@ export const NAV = [
   { title: "Tokens Dashboard", path: "/dashboard" },
   { title: "Buyer", path: "/buyer" },
   { title: "Owner", path: "/owner" },
+  { title: "Verify", path: "/verify"},
 ];
